b25c5c59c1e75b9a70567f2f8eda88c75419b624
Nicholas Hibbits <nicholashibbits1@gmail.com>
2024-07-05 10:12:09 -0700

    add initial actions and reducer function

--- 1-constructing-the-store/index.js
+++ 1-constructing-the-store/index.js
@@ -1,3 +1,68 @@
+const addTodoAction = {
+  type: "ADD_TODO",
+  todo: {
+    id: Math.floor(Math.random(100)),
+    name: "Learn Redux",
+    complete: false,
+  },
+};
+
+const removeTodoAction = {
+  type: "REMOVE_TODO",
+};
+const updateTodoAction = {
+  type: "UPDATE_TODO",
+};
+
+const addGoalAction = {
+  type: "ADD_GOAL",
+};
+const removeGoalAction = {
+  type: "REMOVE_GOAL",
+};
+
+function todos(state = [], action) {
+  const { todos, goals } = state;
+
+  if (action.type === "ADD_TODO") {
+    return {
+      ...state,
+      todos: [...state.todos, action.todo],
+    };
+  }
+  if (action.type === "REMOVE_TODO") {
+    return {
+      ...state,
+      todos: todos.filter((todo) => {
+        todo.id !== action.id;
+      }),
+    };
+  }
+  if (action.type === "UPDATE_TODO") {
+    return {
+      ...state,
+      todos: todos.map((todo) => {
+        if (todo.id !== action.todo.id) return;
+        return (todo = action.todo);
+      }),
+    };
+  }
+  if (action.type === "ADD_GOAL") {
+    return {
+      ...state,
+      goals: [...goals, action.goal],
+    };
+  }
+  if (action.type === "REMOVE_GOAL") {
+    return {
+      ...state,
+      goals: goals.filter((goal) => {
+        goal.id !== action.id;
+      }),
+    };
+  }
+}
+
 function createStore() {
   // store should have 4 parts
   // 1. The state
@@ -20,10 +85,20 @@ function createStore() {
     };
   };
 
+  const dispatch = (action) => {
+    state = todos(state, action);
+    listeners.forEach((listener) => listener());
+  };
+
   return {
     getState,
     subscribe,
+    dispatch,
   };
 }
 
 const store = createStore();
+const unsubscribe = store.subscribe(() => {
+  console.log("The new state is", store.getState());
+});
+store.dispatch(addTodoAction);
