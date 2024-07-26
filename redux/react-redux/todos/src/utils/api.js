28f60fe601a52a0bd65b771b85e204a9b60b17fb
nickhibbits <nickhibbits@gmail.com>
2024-07-26 12:06:31 -0700

    refactor api

--- redux/react-redux/todos/src/utils/api.js
+++ redux/react-redux/todos/src/utils/api.js
@@ -1,124 +1,113 @@
-function fail() {
-  return Math.floor(Math.random() * (5 - 1)) === 3;
-}
-
-function generateId() {
-  return Math.random().toString(36).substring(2);
-}
-
-const goals = [
-  {
-    id: generateId(),
-    name: "Learn Redux",
+export const API = {
+  fail: () => {
+    return Math.floor(Math.random() * (5 - 1)) === 3;
   },
-  {
-    id: generateId(),
-    name: "Read 50 books this year",
+
+  generateId: () => {
+    return Math.random().toString(36).substring(2);
   },
-];
-const todos = [
-  {
-    id: generateId(),
-    name: "Walk the dog",
-    complete: false,
+
+  goals: [
+    {
+      id: () => this.generateId(),
+      name: "Learn Redux",
+    },
+    {
+      id: () => this.generateId(),
+      name: "Read 50 books this year",
+    },
+  ],
+  todos: [
+    {
+      id: () => this.generateId(),
+      name: "Walk the dog",
+      complete: false,
+    },
+    {
+      id: () => this.generateId(),
+      name: "Wash the car",
+      complete: false,
+    },
+    {
+      id: () => this.generateId(),
+      name: "Go to the gym",
+      complete: true,
+    },
+  ],
+
+  fetchGoals() {
+    return new Promise((res, rej) => {
+      // setTimeout(() => {
+      setTimeout(() => {
+        res(this.goals);
+      }, 2000);
+    });
   },
-  {
-    id: generateId(),
-    name: "Wash the car",
-    complete: false,
+
+  fetchTodos() {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        res(this.todos);
+      }, 2000);
+    });
   },
-  {
-    id: generateId(),
-    name: "Go to the gym",
-    complete: true,
+
+  saveTodo: () => {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        const todo = {
+          id: generateId(),
+          name: name,
+          complete: false,
+        };
+        todos = todos.concat([todo]);
+        fail() ? rej(todo) : res(todo);
+      }, 300);
+    });
   },
-];
 
-fetchGoals = function () {
-  return new Promise((res, rej) => {
-    setTimeout(function () {
-      res(goals);
-    }, 2000);
-  });
-};
+  saveGoal: (name) => {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        const goal = {
+          id: generateId(),
+          name: name,
+        };
+        goals = goals.concat([goal]);
+        fail() ? rej(goal) : res(goal);
+      }, 300);
+    });
+  },
 
-fetchTodos = function () {
-  return new Promise((res, rej) => {
-    setTimeout(function () {
-      res(todos);
-    }, 2000);
-  });
-};
+  deleteGoal: (id) => {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        goals = goals.filter((goal) => goal.id !== id);
+        fail() ? rej() : res(goals);
+      }, 300);
+    });
+  },
 
-saveTodo = function (name) {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      const todo = {
-        id: generateId(),
-        name: name,
-        complete: false,
-      };
-      todos = todos.concat([todo]);
-      fail() ? rej(todo) : res(todo);
-    }, 300);
-  });
-};
+  deleteTodo: (id) => {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        todos = todos.filter((todo) => todo.id !== id);
+        fail() ? rej() : res(todos);
+      }, 300);
+    });
+  },
 
-saveGoal = function (name) {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      const goal = {
-        id: generateId(),
-        name: name,
-      };
-      goals = goals.concat([goal]);
-      fail() ? rej(goal) : res(goal);
-    }, 300);
-  });
-};
+  saveTodoToggle: (id) => {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        todos = todos.map((todo) =>
+          todo.id !== id
+            ? todo
+            : Object.assign({}, todo, { complete: !todo.complete })
+        );
 
-deleteGoal = function (id) {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      goals = goals.filter((goal) => goal.id !== id);
-      fail() ? rej() : res(goals);
-    }, 300);
-  });
-};
-
-deleteTodo = function (id) {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      todos = todos.filter((todo) => todo.id !== id);
-      fail() ? rej() : res(todos);
-    }, 300);
-  });
-};
-
-saveTodoToggle = function (id) {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      todos = todos.map((todo) =>
-        todo.id !== id
-          ? todo
-          : Object.assign({}, todo, { complete: !todo.complete })
-      );
-
-      fail() ? rej() : res(todos);
-    }, 300);
-  });
-};
-
-export default {
-  todos,
-  goals,
-  fail,
-  generateId,
-  fetchGoals,
-  fetchTodos,
-  saveTodo,
-  saveGoal,
-  deleteGoal,
-  deleteTodo,
-  saveTodoToggle,
+        fail() ? rej() : res(todos);
+      }, 300);
+    });
+  },
 };
